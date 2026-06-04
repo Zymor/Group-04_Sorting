3be225d39f2f4b203d0603c3b376a52c34418bb4
yunn27-spec <chhingyunn432@gmail.com>
2026-06-04 12:02:28 +0700

    bubble sort

--- Vanny_algorithm.java
+++ Vanny_algorithm.java
@@ -1,3 +1,23 @@
 public class Vanny_algorithm {
-    
+    public static void main(String[] args) {
+
+        int[] numbers = {4, 37, 78, -1, -45, 168};
+
+        // Bubble Sort
+        for (int i = 0; i < numbers.length - 1; i++) {
+            for (int j = 0; j < numbers.length - 1 - i; j++) {
+                if (numbers[j] > numbers[j + 1]) {
+                    int temp = numbers[j];
+                    numbers[j] = numbers[j + 1];
+                    numbers[j + 1] = temp;
+                }
+            }
+        }
+
+        // Result
+        System.out.println("Sorted numbers:");
+        for (int number : numbers) {
+            System.out.print(number + " ");
+        }
+    }
 }
