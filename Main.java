7eac1b6e51f6869075644741ed341cd390b2874d
Starling <nakboyloy50@gmail.com>
2026-06-12 13:11:33 +0700

    update

--- Main.java
+++ Main.java
@@ -4,6 +4,15 @@ public class Main {
     public static void main(String[] args) {
         Phannang_algorithm obj = new Phannang_algorithm();
         obj.standSort();
+        nak obj1 = new nak();
+        obj1.exchangeSort();
+        Phaleng_algorithm obj2 = new Phaleng_algorithm();
+        obj2.insertionSort();
+        Davy_algorithm obj3 = new Davy_algorithm();
+        obj3.selectionSort();
         System.out.println(Arrays.toString(obj.arr));
+        System.out.println(Arrays.toString(obj1.arr));
+        System.out.println(Arrays.toString(obj2.arr));
+        System.out.println(Arrays.toString(obj3.arr));
     }
 }
